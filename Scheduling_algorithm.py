46213c5a30780570ce4737c6121e3a10e5600272
Ayush Kumar <ayushrauniyar785@gmail.com>
2025-04-04 11:41:18 +0530

    completed algorithm

--- Scheduling_algorithm.py
+++ Scheduling_algorithm.py
@@ -14,7 +14,7 @@ def calculate_metrics(timeline, processes):
         pid = p['id']
         turnaround_time[pid] = completion_time[pid] - p['arrival_time']
         waiting_time[pid] = turnaround_time[pid] - p['burst_time']
-      
+    
     avg_turnaround = sum(turnaround_time.values()) / n
     avg_waiting = sum(waiting_time.values()) / n
     
@@ -93,5 +93,77 @@ def RoundRobin(processes, quantum):
         remaining_processes.remove((pid, burst, arrival))
         if remaining_burst > 0:
             remaining_processes.append((pid, remaining_burst, arrival))
+    
+    return calculate_metrics(timeline, processes)
+
+def SRTF(processes):
+    timeline = []
+    current_time = 0
+    remaining_processes = [(p['id'], p['burst_time'], p['arrival_time'], p['burst_time']) for p in processes]
+    current_process = None
+    start_time = current_time
+    
+    while remaining_processes:
+        # Update available processes at current time
+        available = [p for p in remaining_processes if p[2] <= current_time]
+        if not available:
+            current_time = min(p[2] for p in remaining_processes)
+            continue
+        
+        # Sort by remaining time (p[3] is remaining time)
+        next_process = min(available, key=lambda x: x[3])
+        
+        # If different from current process, add current process to timeline
+        if current_process is None or current_process[0] != next_process[0]:
+            if current_process is not None:
+                timeline.append({
+                    'process': current_process[0],
+                    'start': start_time,
+                    'end': current_time
+                })
+            current_process = next_process
+            start_time = current_time
+        
+        # Execute for 1 time unit
+        current_time += 1
+        remaining_time = next_process[3] - 1
+        remaining_processes.remove(next_process)
+        
+        if remaining_time > 0:
+            # Update process with new remaining time
+            remaining_processes.append((next_process[0], next_process[1], next_process[2], remaining_time))
+        else:
+            # Process completed
+            timeline.append({
+                'process': next_process[0],
+                'start': start_time,
+                'end': current_time
+            })
+            current_process = None
+    
+    return calculate_metrics(timeline, processes)
+#Alorithm of Priority 
+def Priority(processes):
+    timeline = []
+    current_time = 0
+    remaining_processes = [(p['id'], p['burst_time'], p['arrival_time'], p['priority']) for p in processes]
+    
+    while remaining_processes:
+        available = [p for p in remaining_processes if p[2] <= current_time]
+        if not available:
+            current_time = min(p[2] for p in remaining_processes)
+            continue
+        
+        # Select process with highest priority (lower number = higher priority)
+        next_process = min(available, key=lambda x: (x[3], x[2]))  # Break ties with arrival time
+        
+        timeline.append({
+            'process': next_process[0],
+            'start': current_time,
+            'end': current_time + next_process[1]
+        })
+        
+        current_time += next_process[1]
+        remaining_processes.remove(next_process)
     
     return calculate_metrics(timeline, processes)
